21e15f6171e8cbbff524f76496c60b95111a017a
TMiyazaki <kd1285579@st.kobedenshi.ac.jp>
2022-08-11 23:35:46 +0900

    Add RectangleI struct.

---
+++ Src/Runtime/Core/Math/RectangleI.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+namespace Miyadaiku
+{
+class RectangleI
+{
+public:
+	int left;
+	int top;
+	int width;
+	int height;
+
+	RectangleI();
+	~RectangleI()
+	{
+	}
+};
+
+RectangleI::RectangleI() 
+	: 
+	left(0),
+	top(0),
+	width(0),
+	height(0)
+{
+}
+} // namespace Miyadaiku
---
+++ Src/Runtime/Test/Math_RectangleI_Test.cpp
@@ -0,0 +1,6 @@
+﻿#include <gtest/gtest.h>
+#include "../Core/Math/Vector2I.h"
+#include "../Core/Math/RectangleI.h"
+#include "TestUtils_Math.h"
+
+
