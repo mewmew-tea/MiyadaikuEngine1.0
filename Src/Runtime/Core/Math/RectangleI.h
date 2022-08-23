245179933c0d39701315c8a1d1eefa9dc6f40710
TMiyazaki <kd1285579@st.kobedenshi.ac.jp>
2022-08-23 10:57:31 +0900

    Add constructors.

--- Src/Runtime/Core/Math/RectangleI.h
+++ Src/Runtime/Core/Math/RectangleI.h
@@ -10,21 +10,18 @@ public:
 	int width;
 	int height;
 
-	RectangleI();
-	RectangleI(int _left, int _top, int _width, int _height);
+	RectangleI() : left(0), top(0), width(0), height(0)
+	{
+	}
+	RectangleI(int _left, int _top, int _width, int _height)
+	{
+		left = _left;
+		top = _top;
+		width = _width;
+		height = _height;
+	}
 	~RectangleI()
 	{
 	}
 };
-
-RectangleI::RectangleI() : left(0), top(0), width(0), height(0)
-{
-}
-RectangleI::RectangleI(int _left, int _top, int _width, int _height)
-{
-	left = _left;
-	top = _top;
-	width = _width;
-	height = _height;
-}
 } // namespace Miyadaiku
