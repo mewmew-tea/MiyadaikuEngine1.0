a5c5086317d300e9e40f34662905a65b4c9780b2
TMiyazaki <kd1285579@st.kobedenshi.ac.jp>
2022-09-01 15:10:05 +0900

    Move definitions of  math common functions.

--- Src/Runtime/Core/Math/Math.cpp
+++ Src/Runtime/Core/Math/Math.cpp
@@ -2,4 +2,12 @@
 
 namespace Miyadaiku
 {
+float ToRadians(float _degrees)
+{
+	return _degrees * (PI / 180.0f);
 }
+float ToDegrees(float _radians)
+{
+	return _radians * (180.0f / PI);
+}
+} // namespace Miyadaiku
--- Src/Runtime/Core/Math/Math.h
+++ Src/Runtime/Core/Math/Math.h
@@ -10,13 +10,7 @@ static const float PI2 = 6.283185307f;
 // PI / 2
 static const float PIDiv2 = 0.159154943f;
 
-float ToRadians(float _degrees)
-{
-	return _degrees * (PI / 180.0f);
-}
+float ToRadians(float _degrees);
 
-float ToDegrees(float _radians)
-{
-	return _radians * (180.0f / PI);
-}
+float ToDegrees(float _radians);
 }
