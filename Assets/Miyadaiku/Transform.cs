95864291f35c45134518baa3b53cc0264c9efe64
mewmew-tea <kd1285579@st.kobedenshi.ac.jp>
2022-10-30 21:06:51 +0900

    Add transform

---
+++ Assets/Miyadaiku/Transform.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiyadaikuEngine
+{
+    // TODO this should be Hierarchical
+    public class Transform
+    {
+        public Vector3 Positon { get { return LocalPositon; } set { LocalPositon = value; } }
+        public Vector3 LocalPositon { get; set; } = new Vector3();
+
+        public Quaternion Rotation { get { return LocalRotation; } set { LocalRotation = value; } }
+        public Quaternion LocalRotation { get; set; } = Quaternion.Identity;
+
+        public Vector3 eulerAngles
+        {
+            get { return Rotation.ToEulerAngles(); }
+            set { Rotation = Quaternion.CreateFromEulerAngles(value); }
+        }
+        public Vector3 LocalEulerAngles
+        {
+            get { return LocalRotation.ToEulerAngles(); }
+            set { LocalRotation = Quaternion.CreateFromEulerAngles(value); }
+        }
+
+        public Vector3 Scale { get { return LocalScale; } set { LocalScale = value; } }
+        public Vector3 LocalScale { get; set; } = new Vector3();
+
+        public Matrix WorldMatrix
+        {
+            get 
+            {
+                Matrix m = Matrix.Identity;
+                m *= Matrix.CreateScale(Scale);
+                m *= Matrix.CreateFromQuaternion(Rotation);
+                m *= Matrix.CreateTranslation(Positon);
+                return m;
+            }
+            set { WorldMatrix = value; }
+        }
+    }
+}
